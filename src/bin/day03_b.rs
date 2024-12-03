2d5724b3d55b6d1f98623a83f18c1dea4415d9cb
Frank Benkstein <frank@benkstein.net>
2024-12-03 08:31:08 +0100

    day 3, part 2

---
+++ src/bin/day03_b.rs
@@ -0,0 +1,38 @@
+use regex::Regex;
+use std::io::Read;
+
+use eyre::Result;
+
+fn read_input() -> Result<String> {
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn solve(input: String) -> i64 {
+    let mul_regex = Regex::new(r"(?P<off>don't)|(?P<on>do)|(?P<op>mul[(](?P<first>\d+),(?P<second>\d+)[)])").unwrap();
+    let mut result = 0;
+    let mut on_off = true;
+
+    for m in mul_regex.captures_iter(&input) {
+        if m.name("on").is_some() {
+            on_off = true;
+        } else if m.name("off").is_some() {
+            on_off = false;
+        } else if on_off && m.name("op").is_some() {
+            let first: i64 = m.name("first").unwrap().as_str().parse().unwrap();
+            let second: i64 = m.name("second").unwrap().as_str().parse().unwrap();
+            result += first * second;
+        }
+    }
+
+    result
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let input = read_input()?;
+    let answer = solve(input);
+    println!("{answer}");
+    Ok(())
+}
